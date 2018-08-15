8cd26a44ea470d4b62926853eca69853f06c570a
meq <fewell@gmail.com>
2018-08-15 12:26:57 -0400

    Added react router

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
     resources :form_responses
   end
 
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
+
 end
