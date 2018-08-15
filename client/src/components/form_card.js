73d8cd754b1a98a52646b59eb6503d30312aa0e7
meq <fewell@gmail.com>
2018-08-15 16:34:05 -0400

    Improved index styling

--- client/src/components/form_card.js
+++ client/src/components/form_card.js
@@ -1,69 +1,95 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Icon, Label, Grid, Menu, Card, Dropdown } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import {
+  Button,
+  Icon,
+  Label,
+  Grid,
+  Menu,
+  Card,
+  Dropdown
+} from "semantic-ui-react";
 
 class FormCard extends Component {
-  state = {like: this.props.form.like}
+  state = { like: this.props.form.like };
 
   handleLike = () => {
-    console.log('before add like', this.state.like)
-    this.addLike(this.state.like+1)
-  }
+    console.log("before add like", this.state.like);
+    this.addLike(this.state.like + 1);
+  };
 
   addLike(value) {
     fetch(`/api/forms/${this.props.form.id}`, {
-        method: 'PUT',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({like: value})})
-        .then(response => response.json())
-        .then(json => {
-          this.setState({ like: json.like})
-          console.log('after put add like', this.state.like)
-        })
-    }
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ like: value })
+    })
+      .then(response => response.json())
+      .then(json => {
+        this.setState({ like: json.like });
+        console.log("after put add like", this.state.like);
+      });
+  }
 
-  render () {
+  render() {
     return (
-      <Grid.Column>
-        <Card>
-          <Card.Content as={Link} to={`forms/edit/${this.props.form.id}`}>
-            <Card.Header>{this.props.form.name}</Card.Header>
-            <Card.Meta> {'questions' in this.props.form ? this.props.form.questions.length : 0} Questions
-            </Card.Meta>
-            <Card.Description>
-            {this.props.form.description}
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
+      <Card>
+        <Card.Content as={Link} to={`forms/edit/${this.props.form.id}`}>
+          <Card.Header>{this.props.form.name}</Card.Header>
+          <Card.Meta>
+            {" "}
+            {"questions" in this.props.form
+              ? this.props.form.questions.length
+              : 0}{" "}
+            Questions
+          </Card.Meta>
+          <Card.Description>{this.props.form.description}</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
           <Menu secondary>
-              <Menu.Item>
-                {'form_responses' in this.props.form ? this.props.form.form_responses.length : 0} Responses
-              </Menu.Item>
-              <Menu.Menu position='right'>
-               <Dropdown item icon='ellipsis horizontal' simple>
-                 <Dropdown.Menu>
-                   <Dropdown.Item as={Link} to={`forms/edit/${this.props.form.id}`}>Edit</Dropdown.Item>
-                   <Dropdown.Item as={Link} to={`forms/${this.props.form.id}`}>Preview</Dropdown.Item>
-                   <Dropdown.Item as={Link} to={`forms/${this.props.form.id}/responses`}>Results</Dropdown.Item>
-                 </Dropdown.Menu>
-               </Dropdown>
-             </Menu.Menu>
-           </Menu>
-           <Menu secondary>
-             <Button as='div' labelPosition='right' onClick={this.handleLike}>
-               <Button icon>
-                 <Icon name='heart' />
-                 Like
-               </Button>
-               <Label as='a' basic pointing='left'>
-                  {this.state.like > 0 ? this.state.like : 0 }
-                </Label>
-             </Button>
+            <Menu.Item>
+              {"form_responses" in this.props.form
+                ? this.props.form.form_responses.length
+                : 0}{" "}
+              Responses
+            </Menu.Item>
+            <Menu.Menu position="right">
+              <Dropdown item icon="ellipsis horizontal" simple>
+                <Dropdown.Menu>
+                  <Dropdown.Item
+                    as={Link}
+                    to={`forms/edit/${this.props.form.id}`}
+                  >
+                    Edit
+                  </Dropdown.Item>
+                  <Dropdown.Item as={Link} to={`forms/${this.props.form.id}`}>
+                    Preview
+                  </Dropdown.Item>
+                  <Dropdown.Item
+                    as={Link}
+                    to={`forms/${this.props.form.id}/responses`}
+                  >
+                    Results
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Menu.Menu>
           </Menu>
-          </Card.Content>
-        </Card>
-      </Grid.Column>
-  )}
+          <Menu secondary>
+            <Button as="div" labelPosition="right" onClick={this.handleLike}>
+              <Button icon>
+                <Icon name="heart" />
+                Like
+              </Button>
+              <Label as="a" basic pointing="left">
+                {this.state.like > 0 ? this.state.like : 0}
+              </Label>
+            </Button>
+          </Menu>
+        </Card.Content>
+      </Card>
+    );
+  }
 }
 
-  export default FormCard
+export default FormCard;
--- client/src/containers/FormsIndex.js
+++ client/src/containers/FormsIndex.js
@@ -1,133 +1,136 @@
-import _ from 'lodash';
-import { bindActionCreators } from 'redux';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Field, reduxForm } from 'redux-form';
-import { fetchForms, createForm, toggleHidden } from '../actions';
-import { Card, Container, Grid, Divider, Button, Menu, Loader, Form } from 'semantic-ui-react';
-import HeaderMenu from '../components/header_menu';
-import SecondaryMenu from '../components/secondary_menu';
-import FormCard from '../components/form_card';
+import _ from "lodash";
+import { bindActionCreators } from "redux";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Field, reduxForm } from "redux-form";
+import { fetchForms, createForm, toggleHidden } from "../actions";
+import {
+  Card,
+  Container,
+  Grid,
+  Divider,
+  Button,
+  Menu,
+  Loader,
+  Form
+} from "semantic-ui-react";
+import HeaderMenu from "../components/header_menu";
+import SecondaryMenu from "../components/secondary_menu";
+import FormCard from "../components/form_card";
 
 class FormsIndex extends Component {
+  handleUpVote = () => {
+    this.setState({ votes: this.state.votes + 1 });
+  };
 
-  handleUpVote = () => {this.setState({ votes: this.state.votes+1 })}
-
-  constructor(props) {
-    super(props)
-    this.onHandleToggle = this.onHandleToggle.bind(this)
-  }
   componentDidMount() {
     this.props.fetchForms();
   }
 
-  renderNewFormCard(){
-    return(
-    <Grid.Column>
-    <Card>
-    <Card.Content>
-      <Card.Header>New Form</Card.Header>
-      <form onSubmit={this.props.handleSubmit(this.onSubmit.bind(this))}>
-        <Form size='large'>
-        <Form.Group>
-        <Form.Field >
-          <Field
-            label="Name:"
-            name="name"
-            component={this.renderField}
-            />
-            </Form.Field>
-            <Form.Field>
-          <div className="field">
-            <label>Description:</label>
-            <Field
-              label=""
-              className
-              name="description"
-              component="textarea"
-            />
-          </div>
-          </Form.Field>
-          </Form.Group>
-          <Divider/>
-        <Grid centered>
-          <Button type="submit" className="btn btn-primary">Submit</Button>
-        </Grid>
-        </Form>
-      </form>
-      </Card.Content>
-    </Card>
-    </Grid.Column>
-  )}
+  renderNewFormCard() {
+    return (
+      <Card>
+        <Card.Content>
+          <Card.Header>New Form</Card.Header>
+          <form onSubmit={this.props.handleSubmit(this.onSubmit.bind(this))}>
+            <Form size="large">
+              <Form.Group>
+                <Form.Field>
+                  <Field
+                    label="Name:"
+                    name="name"
+                    component={this.renderField}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <div className="field">
+                    <label>Description:</label>
+                    <Field
+                      label=""
+                      className
+                      name="description"
+                      component="textarea"
+                    />
+                  </div>
+                </Form.Field>
+              </Form.Group>
+              <Divider />
+              <Grid centered>
+                <Button type="submit" className="btn btn-primary">
+                  Submit
+                </Button>
+              </Grid>
+            </Form>
+          </form>
+        </Card.Content>
+      </Card>
+    );
+  }
 
-  renderForms(){
-    let sortedForms = [...this.props.forms].sort(function (a, b) {
+  renderForms() {
+    let sortedForms = [...this.props.forms].sort(function(a, b) {
       return b.like - a.like;
     });
     return _.map(sortedForms, form => {
-      return <FormCard form={form} key={form.id}/>
+      return <FormCard form={form} key={form.id} />;
     });
   }
 
   renderField(field) {
     return (
       <div className="form-group">
-            <label>{field.label} </label>
-            <input
-            className="form-control"
-            type="text"
-            {...field.input}
-            />
+        <label>{field.label} </label>
+        <input className="form-control" type="text" {...field.input} />
       </div>
     );
   }
 
   onSubmit(values) {
-    this.onHandleToggle()
+    this.onHandleToggle();
     this.props.reset();
-    this.props.createForm(values)
+    this.props.createForm(values);
   }
 
-  //onHandleToggle = () => {}
+  onHandleToggle = () => {
+    const toggle = !this.props.ui;
+    this.props.toggleHidden(toggle);
+  };
 
-  onHandleToggle() {
-    const toggle = !this.props.ui
-    this.props.toggleHidden(toggle)
-  }
-
-  render () {
-    // const { handleSubmit } = this.props;
-    const newFormUrl = 'forms/new';
-    const newFormString = 'New Form';
+  render() {
+    const newFormUrl = "forms/new";
+    const newFormString = "New Form";
 
     if (!this.props.forms) {
       return <Loader />;
     }
     return (
-        <div>
-        <HeaderMenu firstUrl={newFormUrl} firstString={newFormString}/>
-        <Container fluid>
-        <Grid columns='equal'>
-          <Grid.Column width={3}>
-          <Menu secondary>
-          <Menu.Menu position='right'>
-          <Menu.Item>
-            <Button onClick={this.onHandleToggle} icon="plus" content="New Form"  color="teal"/>
-          </Menu.Item>
-          </Menu.Menu>
-          </Menu>
+      <div>
+        <HeaderMenu />
+        <Grid>
+          <Grid.Column width={2}>
+            <Menu secondary>
+              <Menu.Menu position="right">
+                <Menu.Item>
+                  <Button
+                    onClick={this.onHandleToggle}
+                    icon="plus"
+                    content="New Form"
+                    color="teal"
+                  />
+                </Menu.Item>
+              </Menu.Menu>
+            </Menu>
           </Grid.Column>
-          <Grid.Column>
-          <SecondaryMenu firstUrl={newFormUrl} firstString={newFormString}/>
-          <Grid.Column width={10}>
-          <Grid doubling columns={5}>
-              {this.renderForms()}
-              {this.props.ui && this.renderNewFormCard()}
-            </Grid>
-            </Grid.Column>
+          <Grid.Column width={13}>
+            <SecondaryMenu />
+            <Container fluid>
+              <Card.Group>
+                {this.props.ui && this.renderNewFormCard()}
+                {this.renderForms()}
+              </Card.Group>
+            </Container>
           </Grid.Column>
         </Grid>
-      </Container>
       </div>
     );
   }
@@ -140,19 +143,22 @@ function mapStateToProps(state) {
   };
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({
-    fetchForms: fetchForms,
-    createForm: createForm,
-    toggleHidden: toggleHidden
-  }, dispatch);
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      fetchForms: fetchForms,
+      createForm: createForm,
+      toggleHidden: toggleHidden
+    },
+    dispatch
+  );
 };
 
 FormsIndex = connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(FormsIndex);
 
 export default reduxForm({
-  form: 'FormNewForm' // a unique name for this form
+  form: "FormNewForm" // a unique name for this form
 })(FormsIndex);
